54cf9c329bf9f3fe96f2ce896c62e49c749dad68
Gia4a <giaanluu@gmail.com>
2025-08-18 18:32:08 -0600

    firebase start

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'prototype',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'prototype',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
